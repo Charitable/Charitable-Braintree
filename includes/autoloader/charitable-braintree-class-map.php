644388440392cec7848746881eb3cfec8ff50a98
Eric Daams <eric@164a.com>
2020-08-10 09:45:58 +0930

    Added classmap

---
+++ includes/autoloader/charitable-braintree-class-map.php
@@ -0,0 +1,19 @@
+<?php
+/* CLASSMAP: IGNORE */
+return [
+	'Charitable_Braintree_Gateway_Processor' => 'abstracts/class-charitable-braintree-gateway-processor.php',
+	'Charitable_Braintree_Admin' => 'admin/class-charitable-braintree-admin.php',
+	'Charitable_Extension_Activation' => 'admin/class-charitable-extension-activation.php',
+	'Charitable_Braintree' => 'class-charitable-braintree.php',
+	'Charitable_Braintree_Deprecated' => 'deprecated/class-charitable-braintree-deprecated.php',
+	'Charitable_Braintree_Fields' => 'fields/class-charitable-braintree-fields.php',
+	'Charitable_Braintree_Gateway_Processor_One_Time' => 'gateway/class-charitable-braintree-gateway-processor-one-time.php',
+	'Charitable_Braintree_Gateway_Processor_Recurring' => 'gateway/class-charitable-braintree-gateway-processor-recurring.php',
+	'Charitable_Braintree_Plans' => 'gateway/class-charitable-braintree-plans.php',
+	'Charitable_Braintree_Webhook_Processor' => 'gateway/class-charitable-braintree-webhook-processor.php',
+	'Charitable_Gateway_Braintree' => 'gateway/class-charitable-gateway-braintree.php',
+	'Charitable_Braintree_i18n' => 'i18n/class-charitable-braintree-i18n.php',
+	'Charitable_Braintree_Gateway_Processor_Interface' => 'interfaces/interface-charitable-braintree-gateway-processor.php',
+	'Charitable_Braintree_Template' => 'public/class-charitable-braintree-template.php',
+	'Charitable_Braintree_Upgrade' => 'upgrades/class-charitable-braintree-upgrade.php',
+];
